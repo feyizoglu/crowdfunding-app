5a524d6918c346db2562fe179137665fce1a1331
Medetcan Diler <medetdiler1996@gmail.com>
2023-06-27 00:54:47 +0300

    The project is ready to start

--- app/page.js
+++ app/page.js
@@ -1,8 +1,6 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className='grid place-content-center h-screen text-[#020202]'>
+    <main className='grid place-content-center h-screen text-[#020202] animate-pulse'>
       <h1 className='text-3xl text-[#f2f2f2] font-serif'>Hello World. We will have our capstone project here in the near future..</h1>
     </main>
   )
