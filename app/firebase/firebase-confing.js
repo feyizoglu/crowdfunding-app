0d17636872362db83937d112df89daf1370a2395
Medetcan Diler <medetdiler1996@gmail.com>
2023-07-09 22:47:32 +0300

    Updated firebase-config.js file

--- app/firebase/firebase-confing.js
+++ app/firebase/firebase-confing.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DATABASEURL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCxKJ0h4RrBE4J2MKgS8u1DyH3LzmhOEqg",
+  authDomain: "crownfunding-app.firebaseapp.com",
+  databaseURL: "https://crownfunding-app-default-rtdb.firebaseio.com",
+  projectId: "crownfunding-app",
+  storageBucket: "crownfunding-app.appspot.com",
+  messagingSenderId: "849371976837",
+  appId: "1:849371976837:web:05fc8e550c7f25a27985ec"
 };
 
 const app = initializeApp(firebaseConfig);
