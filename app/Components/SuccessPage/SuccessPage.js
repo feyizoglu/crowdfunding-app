c1955dd774983481ae0c9bbd0e5dcbde23b81c86
aybikeustun <aybikeustun@gmail.com>
2023-07-11 23:29:19 +0300

    loading.js is added the success page is done

--- app/Components/SuccessPage/SuccessPage.js
+++ app/Components/SuccessPage/SuccessPage.js
@@ -17,10 +17,10 @@ const SuccessPage = () => {
             <h2 className='text-2xl font-bold mb-2 text-center'>for supporting us!</h2>
         </div>
       <div className='donation-buttons flex space-x-4 justify-center pb-20'>
-        <Link href="/projects" className='px-5 py-2 hover:bg-blackColor hover:text-whiteColor border border-grayishColor focus:ring-4 focus:outline-none focus:ring-grayishColor font-medium rounded-lg text-sm text-center mr-2 mb-2'>
+        <Link href="/projects" className='px-5 py-2 hover:bg-blackColor hover:text-whiteColor  border border-grayishColor focus:ring-4 focus:outline-none focus:ring-grayishColor font-medium rounded-lg text-sm text-center mr-2 mb-2'>
             Make another donation
         </Link>
-        <Link href="/" className='px-10 py-2 hover:bg-blackColor hover:text-whiteColor  border border-grayishColor focus:ring-4 focus:outline-none focus:ring-grayishColor font-medium rounded-lg text-sm text-center mr-2 mb-2  '>
+        <Link href="/" className='px-10 py-2 hover:bg-blackColor hover:text-whiteColor  border border-grayishColor focus:ring-4 focus:outline-none focus:ring-grayishColor font-medium rounded-lg text-sm text-center mr-2 mb-2'>
           Go to home page
         </Link>
       </div>
---
+++ app/successpage/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SuccessPage from '../Components/SuccessPage/SuccessPage';
+
+export default function DonationSuccessPage() {
+
+    return (
+        <SuccessPage />
+
+    )
+}
+    
