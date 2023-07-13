b4f92d2042799e270277b1741d4da7f12bd2b771
Mira Amer <miraamer@Miras-Air.modem.local>
2023-07-13 20:11:37 +0300

    done

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning={true} className={`${inter.className}`}>
+      <body className={`${inter.className}`}>
         <ReduxProvider>
           <NavbarLayOut />
           {children}
