f4e41c4f39d2a3850feabe7812bbedf5a7ab982b
Mira Amer <miraamer@Miras-Air.modem.local>
2023-07-13 17:51:57 +0300

    done

--- app/Components/HeroSection/HeroSection.js
+++ app/Components/HeroSection/HeroSection.js
@@ -1,40 +1,32 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <div className= "bg-[#D4EE25]">
-      <div className="container relative mx-auto px-4 hero-section flex flex-col lg:flex-row items-center justify-start min-h-screen">
-      <div className="content-column text-center lg:text-left text-black px-4 lg:px-0 lg:w-1/2 z-10">
-        <h1 className="text-4xl lg:text-6xl font-bold text-blackColor mb-7">
-          Givingly
-        </h1>
-        <p className="text-lg lg:text-2xl text-blackColor mb-8">
-          Supporting great causes made easy
-        </p>
-        <p className="text-lg lg:text-2xl text-blackColor mb-8">
-          We helped over 3,500 projects and causes. Sign in today and get your idea kicked off or support others kick off their amazing projects.
-        </p>
-        <button className="button-dark">
-          Start Today
-        </button>
+    <div className="bg-greenColor">
+      <div className="h-screen container relative mx-auto px-4 flex flex-col items-center justify-center  md:flex-row md:justify-start ">
+        <div className="flex flex-col w-3/5 text-center text-blackColor z-10 space-y-10 md:ml-20  md:text-left md:w-1/2 lg:w-1/3 xl:w-1/4">
+          <h1 className="text-5xl font-semibold text-blackColor md:text-[100px]">
+            Givingly
+          </h1>
+          <p className="text-[32px] font-semibold text-blackColor  ">
+            Supporting great causes made easy
+          </p>
+          <p className="font-medium text-[18px] text-blackColor">
+            We helped over 3,500 projects and causes. Sign in today and get your
+            idea kicked off or support others kick off their amazing projects.
+          </p>
+          <div>
+            <Link href='/starttoday' className="button-dark px-10 md:px-16">Start Today</Link>
+          </div>
+        </div>
+        <div>
+          <Image src="/background.png" alt="Background image" fill />
+        </div>
       </div>
-      
-      <div className="absolute lg:w-2/2 flex items-center justify-end">
-        <Image
-          src="/background.png"
-          alt="Background image"
-          width={3000}
-          height={3000}
-        />
-      </div>
-
-    </div>
     </div>
   );
 };
 
-
 export default HeroSection;
-
-
