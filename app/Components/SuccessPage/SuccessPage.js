93cae17891e9a5e5410c13233b4e2f2ce7556957
aybikeustun <aybikeustun@gmail.com>
2023-07-13 22:22:13 +0300

    photo is replaced. div is updated

--- app/Components/SuccessPage/SuccessPage.js
+++ app/Components/SuccessPage/SuccessPage.js
@@ -4,19 +4,19 @@ import Link from 'next/link';
 
 const SuccessPage = () => {
   return (
-    <div className='container mx-auto px-4 flex flex-col h-screen'>
+    <div className='container mx-auto px-4 flex flex-col h-screen items-center justify-center'>
       <Image
-                    src="/donation.jpg"
-                    width={500}
-                    height={500}
+                    src="/thankyou.png"
+                    width={1000}
+                    height={1000}
                     alt='donationImage'
-                    className='mx-auto pb-10 pt-10'
+                    className='mx-auto pt-10'
                   />
-        <div className='py-10'>
+        <div className='pb-10'>
             <h1 className=' text-4xl sm:text-5xl md:text-6xl font-bold mb-4 text-center'>Thank you!</h1>
             <h2 className='text-2xl font-bold mb-2 text-center'>for supporting us!</h2>
         </div>
-      <div className='donation-buttons flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 justify-center pb-20 text-center'>
+      <div className='donation-buttons flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 justify-center text-center'>
         <Link href="/projects" className='button-light'>
             Make another donation
         </Link>
