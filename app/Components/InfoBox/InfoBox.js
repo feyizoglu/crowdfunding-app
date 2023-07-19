0fce1f5049cfff60508565b014c800b955df0c8b
Gürkan <gurkanyigitgr@gmail.com>
2023-07-19 16:01:36 +0300

    add "/myprojectpage" link

--- app/Components/InfoBox/InfoBox.js
+++ app/Components/InfoBox/InfoBox.js
@@ -1,37 +1,37 @@
-import Link from 'next/link';
-import { useSelector } from 'react-redux';
-import { auth } from '@/app/firebase/firebase-confing';
-import { signOut } from 'firebase/auth';
-import { useRouter } from 'next/navigation';
+import Link from "next/link";
+import { useSelector } from "react-redux";
+import { auth } from "@/app/firebase/firebase-confing";
+import { signOut } from "firebase/auth";
+import { useRouter } from "next/navigation";
 
-import { BiUser, BiUserX } from 'react-icons/bi'
-import { HiOutlineMail } from 'react-icons/hi'
-import { RiProfileLine } from 'react-icons/ri'
+import { BiUser, BiUserX } from "react-icons/bi";
+import { HiOutlineMail } from "react-icons/hi";
+import { RiProfileLine } from "react-icons/ri";
 
 function InfoBox({ style }) {
-  const user = useSelector(state => state.auth.user); 
+  const user = useSelector((state) => state.auth.user);
   const router = useRouter();
 
   const signOutHandler = async () => {
     await signOut(auth);
-    router.push('/');
-  }
+    router.push("/");
+  };
 
   return (
-    <div className='relative'>
+    <div className="relative">
       <section className={style.InfoBoxContainer}>
         <div className={`${style.userInfos}`}>
-          <p className={style.infoBoxPTags} >
+          <p className={style.infoBoxPTags}>
             <BiUser size={15} />
-            {user.split('@')[0]}
+            {user.split("@")[0]}
           </p>
-          <p className={style.infoBoxPTags} >
+          <p className={style.infoBoxPTags}>
             <HiOutlineMail size={15} />
             {user}
           </p>
         </div>
         <div className={style.userFeatures}>
-          <Link className={`${style.infoBoxLinks}`} href="#" >
+          <Link className={`${style.infoBoxLinks}`} href="/myprojectpage">
             <RiProfileLine size={15} />
             View your project
           </Link>
@@ -45,7 +45,7 @@ function InfoBox({ style }) {
         <div className="w-3 h-3 bg-grayishColor transform rotate-45 origin-top-right "></div>
       </div>
     </div>
-  )
+  );
 }
 
 export default InfoBox;
