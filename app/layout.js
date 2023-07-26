4a1874f05c8ec8690e88fc4bff71f211aeeb597f
Tolgahan <feyizoglutolgahan@hotmail.com>
2023-07-26 10:52:04 +0300

    Oswald font is applied

--- app/layout.js
+++ app/layout.js
@@ -1,24 +1,26 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Oswald } from "next/font/google";
 
-import NavbarLayOut from './Components/NavbarLayout/NavbarLayout'
-import { ReduxProvider } from './redux/provider'
-import { ToastContainer } from 'react-toastify';
-import ProjectsContainer from './Components/ProjectsContainer/ProjectsContainer';
-import 'react-toastify/dist/ReactToastify.css';
-import Footer from './Components/Footer/Footer';
+import NavbarLayOut from "./Components/NavbarLayout/NavbarLayout";
+import { ReduxProvider } from "./redux/provider";
+import { ToastContainer } from "react-toastify";
+import ProjectsContainer from "./Components/ProjectsContainer/ProjectsContainer";
+import "react-toastify/dist/ReactToastify.css";
+import Footer from "./Components/Footer/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const oswald = Oswald({
+  subsets: ["latin"],
+});
 
 export const metadata = {
-  title: 'Givingly',
-  description: 'The project will be created by team 9 in the near future',
-}
+  title: "Givingly",
+  description: "The project will be created by team 9 in the near future",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>
+      <body className={oswald.className}>
         <ReduxProvider>
           <ProjectsContainer>
             <NavbarLayOut />
@@ -29,5 +31,5 @@ export default function RootLayout({ children }) {
         </ReduxProvider>
       </body>
     </html>
-  )
+  );
 }
