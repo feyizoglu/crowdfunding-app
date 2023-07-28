b390c760d0301de584c9d9265c4cae37c6ba2a22
aybikeustun <aybikeustun@gmail.com>
2023-07-29 00:04:34 +0300

    potw component connected firebase

--- app/[locale]/Components/ProjectOfTheWeek/ProjectOfTheWeek.js
+++ app/[locale]/Components/ProjectOfTheWeek/ProjectOfTheWeek.js
@@ -1,21 +1,32 @@
+'use client'
 import React from "react";
+import { useSelector, useDispatch } from 'react-redux';
 import Image from "next/image";
 import Link from "next/link";
-import projects from "../../../data/projectData";
+import { setSelectedLink } from '@/app/redux/features/authSlice';
+import Loader from "../Loader/Loader";
 
-const projectOfTheWeek = () => {
-  let leastMoneyRaisedProject = projects[0];
+function projectOfTheWeek() {
+  const projects = useSelector(state => state.auth.projects);
+  const dispatch = useDispatch();
 
-  for (let i = 1; i < projects.length; i++) {
-    if (projects[i].moneyRaised < leastMoneyRaisedProject.moneyRaised) {
-      leastMoneyRaisedProject = projects[i];
-    }
+  console.log(projects[0])
+
+  // Check if the projects array is empty or undefined
+  if (!projects || projects.length === 0) {
+    return (
+      <div className="bg-whiteColor h-screen-70">
+        <Loader />
+      </div>
+    );
   }
 
-  const { title, description, image, creator, category, goalAmount, moneyRaised, backers, endDate } = leastMoneyRaisedProject;
+  // Find the most recent project
+  const mostRecentProject = projects.reduce((prev, curr) => (
+    curr.uploadTimestamp > prev.uploadTimestamp ? curr : prev
+  ));
 
-  const progressPercentage = (moneyRaised / goalAmount) * 100;
-
+ 
 
   return (
     <div className='bg-whiteColor text-center md:text-start'>
@@ -23,50 +34,51 @@ const projectOfTheWeek = () => {
         <div className='border-b border-blackColor pb-20'>
           <h1 className="project-of-the-week__title text-4xl sm:text-5xl md:text-6xl font-bold mb-4 pb-10 ">Project of the Week</h1>
           <div className="project-of-the-week__content grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-20 ">
-            <div className="img-container flex flex-col justify-center items-center bg-grayishColor rounded-lg border-0">
-              <Link href="#">
-                <div className="image-wrapper flex justify-center items-center w-full h-full">
-                  <Image
-                    src="/mango.png"
-                    width={500}
-                    height={500}
-                    alt="projectImage"
-                    priority
-                  />
-                </div>
+            <div className="img-container flex flex-col justify-center items-center rounded-lg border-0">
+              <Link href={`/projects/${mostRecentProject.docId}`}>
+               
+                <Image
+                  className="rounded-lg object-cover" 
+                  src={mostRecentProject.image}
+                  width={400}
+                  height={400}
+                  alt="projectImage"
+                  priority
+                />
+                
               </Link>
             </div>
-            <div className="flex flex-col justify-between">
+            <div className="flex flex-col justify-center space-y-16">
               <div className="mb-2">
                 <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-2">
-                  <Link href="#">
-                    {title}
-                  </Link>
+                <Link href={`/projects/${mostRecentProject.docId}`} passHref>
+                {mostRecentProject.title}
+               </Link>
                 </h1>
               </div>
               <div>
                 <p className="mb-2">
-                  {description}
+                  {mostRecentProject.description}
                 </p>
               </div>
               <div className="progress-bar flex flex-col justify-between">
                 <div className="h-4 bg-grayishColor rounded-lg">
                   <div
                     className="h-full rounded-lg bg-greenColor"
-                    style={{ width: `${progressPercentage}%` }}
+                    style={{ width: `${(mostRecentProject.moneyRaised / mostRecentProject.goalAmount) * 100}%` }}
                   />
                 </div>
                 <div className="flex justify-between">
                   <div className="raised flex flex-col justify-between">
                     <p className="text-sm sm:text-base md:text-lg">Raised:</p>
                     <p className="text-lg sm:text-xl md:text-2xl font-bold py-1 sm:py-2 md:py-3">
-                      ${moneyRaised}
+                      ${mostRecentProject.moneyRaised}
                     </p>
                   </div>
                   <div className="goal flex flex-col justify-between">
                     <p className="text-sm sm:text-base md:text-lg">Goal:</p>
                     <p className="text-lg sm:text-xl md:text-2xl font-bold py-1 sm:py-2 md:py-3">
-                      ${goalAmount}
+                      ${mostRecentProject.goalAmount}
                     </p>
                   </div>
                 </div>
@@ -77,7 +89,6 @@ const projectOfTheWeek = () => {
       </div>
     </div>
   );
-};
-
+}
 
 export default projectOfTheWeek;
