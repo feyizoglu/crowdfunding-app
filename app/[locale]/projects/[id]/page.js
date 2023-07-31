980c86da7fb852a4ddd572ae2e04b9f67db206e2
Tolgahan <feyizoglutolgahan@hotmail.com>
2023-08-01 02:00:59 +0300

    Project Page Translation done

--- app/[locale]/projects/[id]/page.js
+++ app/[locale]/projects/[id]/page.js
@@ -5,6 +5,7 @@ import { doc, getDoc } from "firebase/firestore";
 import Image from "next/image";
 import { FaCalendarAlt } from "react-icons/fa";
 import { parse, differenceInDays } from "date-fns";
+import { useTranslations } from "next-intl";
 
 import Loader from "@/app/[locale]/Components/Loader/Loader";
 
@@ -23,19 +24,26 @@ function Page({ params }) {
     };
     fetchData();
   }, [params]);
-
+  const t = useTranslations("Project");
   const today = new Date();
   const endDate = parse(project?.timeline, "dd/MM/yy", new Date());
   const dayLeft = differenceInDays(endDate, today);
-  const message = (() => {
-    if (dayLeft == 1) {
-      return "1 day left";
-    } else if (dayLeft == 0) {
-      return "Ends today";
+  let message;
+  if (t("Lang") === "English") {
+    if (dayLeft === 1) {
+      message = "1 day left";
+    } else if (dayLeft === 0) {
+      message = "Ends today";
     } else {
-      return `${dayLeft} days left`;
+      message = `${dayLeft} days left`;
     }
-  })();
+  } else {
+    if (dayLeft === 0) {
+      message = "Bugün sona eriyor";
+    } else {
+      message = `${dayLeft} gün kaldı`;
+    }
+  }
 
   const progressPercentage = (project?.moneyRaised / project?.goalAmount) * 100;
 
@@ -66,7 +74,7 @@ function Page({ params }) {
             </div>
             <div className="flex flex-col justify-between  md:border-y md:border-blackColor md:flex-row md:space-y-0 md:container">
               <div className="flex flex-col space-y-5 px-2 md:w-1/2 md:pt-5">
-                <h1 className="text-lg font-bold">About project</h1>
+                <h1 className="text-lg font-bold">{t("About Project")}</h1>
                 <p className="text-lg pb-5">{project?.description}</p>
               </div>
               <div className="md:border-l md:border-blackColor "></div>
@@ -74,13 +82,17 @@ function Page({ params }) {
                 <div className="space-y-5 progress-bar flex flex-col justify-between md:px-2  ">
                   <div className="flex justify-between">
                     <div className="raised flex flex-col px-4 py-2 justify-between">
-                      <p className="text-sm sm:text-base md:text-lg">Raised:</p>
+                      <p className="text-sm sm:text-base md:text-lg">
+                        {t("Raised:")}
+                      </p>
                       <p className="text-lg sm:text-xl md:text-xl font-bold py-1 sm:py-2 md:py-3">
                         ${project?.moneyRaised}
                       </p>
                     </div>
                     <div className="bg-greenColor px-6 py-2 rounded-lg flex flex-col justify-between ">
-                      <p className="text-sm sm:text-base md:text-lg">Goal:</p>
+                      <p className="text-sm sm:text-base md:text-lg">
+                        {t("Goal:")}
+                      </p>
                       <p className="text-lg sm:text-xl md:text-xl font-bold py-1 sm:py-2 md:py-3">
                         ${project?.goalAmount}
                       </p>
@@ -101,7 +113,7 @@ function Page({ params }) {
             </div>
             <div>
               <button className="button-dark mt-2 w-full md:w-1/2">
-                Fund This Project
+                {t("Fund This Project")}
               </button>
             </div>
           </div>
