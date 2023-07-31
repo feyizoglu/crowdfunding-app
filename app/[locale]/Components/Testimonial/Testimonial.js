9a377e62378b1b8d562c5d00f34397993c8c35d5
Tolgahan <feyizoglutolgahan@hotmail.com>
2023-07-31 14:58:01 +0300

    Testimonials section translation is done

--- app/[locale]/Components/Testimonial/Testimonial.js
+++ app/[locale]/Components/Testimonial/Testimonial.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import testimonials from "@/app/data/testimonials";
+import { useTranslations } from "next-intl";
 
 const style = {
   card: `flex flex-col justify-center items-center bg-grayishColor max-w-xs text-sm text-center h-80 px-3 relative mb-20  rounded-lg transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 sm:text-lg lg:h-96 xl:h-80`,
@@ -10,6 +11,13 @@ function Testimonial({ id }) {
   const testimonialData = testimonials.find(
     (testimonial) => testimonial.id === id
   );
+  const t = useTranslations("Testimonial");
+  let content, profession;
+  t("Lang") == "English"
+    ? (content = testimonialData.content) &&
+      (profession = testimonialData.profession)
+    : (content = testimonialData.contentTr) &&
+      (profession = testimonialData.professionTr);
 
   return (
     <div className={style.card}>
@@ -21,14 +29,14 @@ function Testimonial({ id }) {
         className="rounded-xl  -top-12 absolute border-2 border-whiteColor"
       />
       <div className="flex flex-col justify-around items-center">
-        <p className="px-4 pt-10">{testimonialData.content}</p>
+        <p className="px-4 pt-10">{content}</p>
         <div className="flex pt-3 md:text-lg">
           <div className="pr-2 bold">
             <h2>{testimonialData.author}</h2>
           </div>
           <div className="border-l-2 border-blackColor pr-2"></div>
           <div>
-            <p className="text-blueColor">{testimonialData.profession}</p>
+            <p className="text-blueColor">{profession}</p>
           </div>
         </div>
       </div>
