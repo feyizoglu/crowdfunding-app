cc1ff9a4b35ec822e55ed877636e8a76d9e16142
Gürkan <gurkanyigitgr@gmail.com>
2023-08-04 15:15:00 +0300

    translate for signup

--- app/[locale]/signup/page.js
+++ app/[locale]/signup/page.js
@@ -8,30 +8,32 @@ import { auth } from "../../firebase/firebase-confing";
 import { createUserWithEmailAndPassword, AuthErrorCodes } from "firebase/auth";
 import { useDispatch } from "react-redux";
 import { useRouter } from "next/navigation";
-import { toast } from 'react-toastify';
-
+import { toast } from "react-toastify";
 import { db, storage } from "@/app/firebase/firebase-confing";
 import { addDoc, collection } from "firebase/firestore";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
-
+import { useTranslations } from "next-intl";
 
 import { setShowSignInBox } from "../../redux/features/authSlice";
 import Alert from "../Components/SignUpAlert/Alert";
 
-const schema = yup.object().shape({
-  email: yup.string().email("please enter a valid email address.").required(),
-  password: yup
-    .string()
-    .required()
-    .min(6, "asdfasdfasdfsd"),
-  profilePic: yup
-    .mixed()
-    .test("fileRequired", "Image is required", (value) => {
-      return value.length > 0;
-    }),
-});
-
 const Page = () => {
+  const t = useTranslations("SignUp");
+  const schema = yup.object().shape({
+    email: yup
+      .string()
+      .email(`${t("please enter a valid email address")}`)
+      .required(`${t("email is a required field")}`),
+    password: yup
+      .string()
+      .required(`${t("password is a required field")}`)
+      .min(6, `${t("password must be at least 6 characters long")}`),
+    profilePic: yup
+      .mixed()
+      .test("fileRequired", "Image is required", (value) => {
+        return value.length > 0;
+      }),
+  });
   const {
     register,
     handleSubmit,
@@ -46,23 +48,32 @@ const Page = () => {
 
   const onSubmit = async (data) => {
     try {
-      const user = await createUserWithEmailAndPassword(auth, data.email, data.password)
-      route.push('/')
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+      route.push("/");
       const profilePicFile = data.profilePic[0];
       const storageRef = ref(storage, `profil-pic/${profilePicFile.name}`);
       const snapshot = await uploadBytes(storageRef, profilePicFile);
-      const downloadURL = await getDownloadURL(snapshot.ref)
-      await addDoc(collection(db, 'profilPics'), {
+      const downloadURL = await getDownloadURL(snapshot.ref);
+      await addDoc(collection(db, "profilPics"), {
         id: user.user.uid,
-        profilPic: downloadURL
-      })
-      let userName = data.email.split('@')[0]
-      toast.success(`Congratulations ${userName[0].toUpperCase() + userName.slice(1, userName.length)}! Your sign-up was successful. Welcome to our community.`, {
-        position: toast.POSITION.BOTTOM_RIGHT,
-        draggable: false
+        profilPic: downloadURL,
       });
+      let userName = data.email.split("@")[0];
+      toast.success(
+        `Congratulations ${
+          userName[0].toUpperCase() + userName.slice(1, userName.length)
+        }! Your sign-up was successful. Welcome to our community.`,
+        {
+          position: toast.POSITION.BOTTOM_RIGHT,
+          draggable: false,
+        }
+      );
     } catch (err) {
-      let errorMsg = 'default';
+      let errorMsg = "default";
       switch (err.code) {
         case AuthErrorCodes.EMAIL_EXISTS:
           errorMsg = "The email address is already in use by another account.";
@@ -75,37 +86,39 @@ const Page = () => {
       }
       toast.error(errorMsg, {
         position: toast.POSITION.BOTTOM_RIGHT,
-        draggable: false
-      })
+        draggable: false,
+      });
     }
   };
 
   const handleAlreadyMemberClick = () => {
     setTimeout(() => {
-      dispatch(setShowSignInBox())
-    }, 1)
+      dispatch(setShowSignInBox());
+    }, 1);
   };
 
   return (
     <div className="flex flex-col py-20 justify-start items-center mt-[70px] md:justify-center md:h-screen-70">
-      <h2 className="text-4xl font-bold mb-7 text-blackColor">Sign-Up</h2>
+      <h2 className="text-4xl font-bold mb-7 text-blackColor">
+        {t("Sign-Up")}
+      </h2>
       <div className="max-w-2xl w-full  flex flex-col items-center justify-start p-6 bg-whiteColor rounded-md shadow-md">
         <p className="text-md mb-3 max-w-lg text-center">
-          To reach people's needs, to help people, simplify life with
-          <br /> Crownfunding App.
+          {t("To reach people's needs, to help people, simplify life with")}
+          <br /> {t("Crownfunding App")}.
         </p>
         <hr className="bg-blackColor w-24 mt-1 mb-5" />
         <Link
-          href='/'
+          href="/"
           onClick={handleAlreadyMemberClick}
           className="text-sm text-[#0361FD] hover:opacity-60"
         >
-          Already a member? Sign-in
+          {t("Already a member? Sign-in")}
         </Link>
         <form onSubmit={handleSubmit(onSubmit)} className="w-full p-8">
           <div className="mb-4">
             <label htmlFor="email" className="text-xl font-semibold block mb-2">
-              Your personal email address
+              {t("Your personal email address")}
             </label>
             <input
               id="email"
@@ -119,7 +132,7 @@ const Page = () => {
               htmlFor="password"
               className="text-xl font-semibold block mb-2"
             >
-              Password
+              {t("Password")}
             </label>
             <input
               type="password"
@@ -131,14 +144,25 @@ const Page = () => {
           {errors.password && <Alert message={errors.password?.message} />}
           <div className="mb-4">
             <label htmlFor="image" className="text-xl font-semibold block mb-2">
-              Upload Profile Picture
+              {t("Upload Profile Picture")}
             </label>
             <div className="file-input m-5 ml-0">
-              <label htmlFor="file-upload" className={`file-label button-light ${errors.profilePic && `bg-redColor text-red-500 border-red-500 hover:bg-lightRedColor hover:text-red-500 hover:opacity-60`}`}>
-                <FaUpload className={`text-[#0361FD] mr-4 ${errors.profilePic && 'text-darkRedColor'}`} /> Upload Profile Pic
+              <label
+                htmlFor="file-upload"
+                className={`file-label button-light ${
+                  errors.profilePic &&
+                  `bg-redColor text-red-500 border-red-500 hover:bg-lightRedColor hover:text-red-500 hover:opacity-60`
+                }`}
+              >
+                <FaUpload
+                  className={`text-[#0361FD] mr-4 ${
+                    errors.profilePic && "text-darkRedColor"
+                  }`}
+                />{" "}
+                {t("Upload Profile Picture")}
               </label>
               <input
-                {...register('profilePic')}
+                {...register("profilePic")}
                 id="file-upload"
                 type="file"
                 style={{ display: "none" }}
@@ -146,7 +170,7 @@ const Page = () => {
             </div>
           </div>
           <button type="submit" className="w-full h-10 button-dark">
-            Submit
+            {t("Submit")}
           </button>
         </form>
       </div>
